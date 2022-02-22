f2f77ea035ce89ec4c58a992397987f4eff82c11
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-02-23 01:12:15 +0800

    fix: share on desktop and tablet

--- newbie/article-preview-component/assets/js/script.js
+++ newbie/article-preview-component/assets/js/script.js
@@ -5,8 +5,14 @@ const btnShare = document.getElementById("btn-share");
 btnShare.addEventListener("click", function (e) {
     e.stopPropagation();
     share.classList.add("active");
+    this.className = "card__share bg-dark";
+    this.firstElementChild.classList.add("icon-light");
+    this.firstElementChild.classList.remove("icon-dark");
 });
 
 document.addEventListener("click", function () {
     share.classList.remove("active");
+    btnShare.className = "card__share bg-light";
+    btnShare.firstElementChild.classList.add("icon-dark");
+    btnShare.firstElementChild.classList.remove("icon-light");
 });
