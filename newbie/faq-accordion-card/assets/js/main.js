98b247ae6c9e5856ad93d1f93ff34a9a732bfaf4
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-12-12 15:53:49 +0800

    accordion open

--- newbie/faq-accordion-card/assets/js/main.js
+++ newbie/faq-accordion-card/assets/js/main.js
@@ -1,12 +1,33 @@
 /* ==================== ACCORDION ===================== */
-function toggleAccordion(headerSelector) {
+function toggleAccordion(accordionSelector, headerSelector, contentSelector) {
+    const accordion = document.querySelectorAll(accordionSelector);
     const accordionHeader = document.querySelectorAll(headerSelector);
 
     accordionHeader.forEach((ah) => {
         ah.addEventListener("click", function () {
-            this.parentElement.classList.toggle("accordion-open");
+            const parent = this.parentElement;
+
+            if (!parent.classList.contains("accordion-open")) {
+                accordion.forEach((ac) => {
+                    ac.classList.remove("accordion-open");
+                    ac.querySelector(contentSelector).style.height = 0;
+                });
+            }
+
+            parent.classList.toggle("accordion-open");
+
+            const content = this.nextElementSibling;
+            if (parent.classList.contains("accordion-open")) {
+                content.style.height = content.scrollHeight + "px";
+            } else {
+                content.style.height = 0;
+            }
         });
     });
 }
 
-toggleAccordion(".card__accordion-header");
+toggleAccordion(
+    ".card__accordion",
+    ".card__accordion-header",
+    ".card__accordion-content"
+);
