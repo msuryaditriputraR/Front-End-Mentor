c47dafb9d294539dd2a1c97cb3fcc257714467ec
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-12-18 00:37:10 +0800

    handle state

--- intermediate/url-shortening-api/assets/js/main.js
+++ intermediate/url-shortening-api/assets/js/main.js
@@ -1,3 +1,6 @@
+import getShortLink from "./api/getShortLink.js";
+import errorHTML from "./helper/errorHTML.js";
+
 /*==================== MENU SHOW / HIDDEN ====================*/
 const toggleMenu = (toggleId, menuId) => {
     const toggle = document.getElementById(toggleId),
@@ -24,13 +27,45 @@ if (shorterForm) {
 
         // SELECT INPUT
         const input = this.querySelector("#shorter-input");
+        const errorEl = this.querySelector("#error-message");
 
         // CHECK INPUT VALUE
-        if (!input.value) {
-            // CHANGE BORDER OF INPUT
-            input.classList.add("error");
-        } else {
-            input.classList.remove("error");
+        if (validateInput(input.value)) {
+            // GET SHORT URL
+            getShortLink(input.value)
+                .then((response) => response.json())
+                .then((result) => result.result.short_link)
+                .then((sl) => console.log(sl));
+        }
+
+        input.addEventListener("input", function () {
+            // CHECK INPUT VALUE
+            validateInput(this.value);
+        });
+
+        function validateInput(value) {
+            const regex = new RegExp("^https?://");
+
+            if (!value) {
+                // CHANGE BORDER OF INPUT
+                input.classList.add("error");
+                // ADD ERROR MESSAGE
+                errorHTML(errorEl, "Please add a link");
+
+                return false;
+            }
+            // CHECK IF VALUE IS VALID URL
+            else if (!value.match(regex)) {
+                errorHTML(errorEl, "Please enter a valid URL");
+                return false;
+            }
+            // VALUE IS VALID URL
+            else {
+                input.classList.remove("error");
+                errorEl.style.display = "none";
+
+                return true;
+            }
         }
     });
 }
