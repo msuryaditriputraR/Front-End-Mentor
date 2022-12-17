a8257937f8750d1e4a56eda77b8b9d801d4abddf
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-12-17 22:01:36 +0800

    handle state when input is empty

--- intermediate/url-shortening-api/assets/js/main.js
+++ intermediate/url-shortening-api/assets/js/main.js
@@ -13,3 +13,24 @@ const toggleMenu = (toggleId, menuId) => {
 };
 
 toggleMenu("nav-toggle", "nav-menu");
+
+/* ==================== HANDLE SUBMIT ===================== */
+const shorterForm = document.getElementById("shorter-form");
+
+if (shorterForm) {
+    shorterForm.addEventListener("submit", function (e) {
+        // NO RELOAD AFTER SUBMIT CLICKED
+        e.preventDefault();
+
+        // SELECT INPUT
+        const input = this.querySelector("#shorter-input");
+
+        // CHECK INPUT VALUE
+        if (!input.value) {
+            // CHANGE BORDER OF INPUT
+            input.classList.add("error");
+        } else {
+            input.classList.remove("error");
+        }
+    });
+}
