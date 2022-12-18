131c8eb3fb90e55b4e9f5b3763a86b18ab0a069f
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-12-18 10:51:35 +0800

    create card result component

---
+++ intermediate/url-shortening-api/assets/js/component/cardResult.js
@@ -0,0 +1,34 @@
+export default function cardResult(originalLink, shortLink) {
+    const cardClassname = "shorter__card";
+
+    // CREATE P TAG FOR ORIGINAL LINK
+    const originalLinkP = document.createElement("p");
+
+    originalLinkP.className = cardClassname + "-original";
+    originalLinkP.setAttribute("title", originalLink);
+    originalLinkP.innerText = originalLink;
+
+    // CREATE P TAG FOR SHORT LINK
+    const shortLinkP = document.createElement("p");
+
+    shortLinkP.className = cardClassname + "-short";
+    shortLinkP.innerText = shortLink;
+
+    // CREATE HR TAG
+    const hr = document.createElement("hr");
+
+    hr.className = cardClassname + "-hr";
+
+    // CREATE BUTTON COPY
+    const btnCopy = document.createElement("button");
+
+    btnCopy.className = cardClassname + "-btn";
+    btnCopy.innerText = "Copy";
+
+    const card = document.createElement("div");
+
+    card.className = cardClassname;
+    card.append(originalLinkP, hr, shortLinkP, btnCopy);
+
+    return card;
+}
