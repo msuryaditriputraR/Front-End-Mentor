a02fdbaa5e27d03a1ef0aed73236d778c4cc9edc
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-12-19 21:12:49 +0800

    avoid write looping element in html file, refactoring in JS

--- newbie/interactive-rating-component/assets/js/main.js
+++ newbie/interactive-rating-component/assets/js/main.js
@@ -1,3 +1,42 @@
+/* ==================== CREATE RADIOS ===================== */
+
+function createRadio(name, index) {
+    // Create Fragment as a container
+    const fragment = document.createDocumentFragment();
+
+    // create input tag and adding its attribute
+    const input = document.createElement("input");
+    input.type = "radio";
+    input.name = name;
+    input.id = `${name}-${index}`;
+    input.className = "card__radio";
+    input.value = index;
+
+    // create label tag and adding its attribute
+    const label = document.createElement("label");
+    label.innerText = index;
+    label.htmlFor = input.id;
+    label.className = "card__label";
+
+    // Add Input and Label to fragment element
+    fragment.append(input, label);
+
+    return fragment;
+}
+
+// ADD RADIO TO DOM
+
+// Select radios container
+const radios = document.getElementById("card-radios");
+
+// Looping according to the number
+let i = 1;
+while (i <= 5) {
+    // append radio fragment to radios
+    radios.appendChild(createRadio("rating", i));
+    i++;
+}
+
 /* ==================== HANDLE RATING SUBMIt ===================== */
 
 // SELECT ELEMENT
