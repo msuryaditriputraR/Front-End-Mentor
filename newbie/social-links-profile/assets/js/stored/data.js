a9a12ff507b1e599da0d1087f05db0168c7673c9
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2024-08-19 10:16:16 +0800

    add: data.js

---
+++ newbie/social-links-profile/assets/js/stored/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        href: "https://github.com/msuryaditriputraR",
+        icon: "ri-github-fill",
+        text: "Github",
+    },
+    {
+        href: "https://www.linkedin.com/in/msuryaditriputra/",
+        icon: "ri-linkedin-box-fill",
+        text: "LinkedIn",
+    },
+    {
+        href: "https://www.instagram.com/msuryaditriputra",
+        icon: "ri-instagram-fill",
+        text: "Instagram",
+    },
+    {
+        href: "https://www.youtube.com/@msuryaditriputraR",
+        icon: "ri-youtube-fill",
+        text: "Youtube",
+    },
+    {
+        href: "https://medium.com/@msuryaditriputra",
+        icon: "ri-medium-fill",
+        text: "Medium",
+    },
+];
+
+export default data;
